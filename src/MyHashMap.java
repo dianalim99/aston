044401c160daa5fe3c74e491bc797f11b48db36b
Диана <ms.masalimovadiana@gmail.com>
2024-01-22 02:43:48 +0300

    add java doc

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,17 +1,40 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-
+/**
+ * Этот класс представляет собой реализацию HashMap, которая позволяет хранить пары ключ-значение
+ * и обеспечивает быстрый доступ к значениям.
+ */
 public class MyHashMap<K, V> {
+    /**
+     * Значение емкости по умолчанию
+     */
     private static final int DEFAULT_CAPACITY = 16;
+    /**
+     * Значение предельного значения по умолчанию
+     */
     private static final double DEFAULT_LOAD_FACTOR = 0.75;
-
+    /**
+     * Динамический массив для хранения корзин (связанных списков) пар ключ-значение.
+     */
     private ArrayList<LinkedList<Node<K, V>>> buckets;
+    /**
+     * Размер HashMap (количество пар ключ-значение).
+     */
     private int size;
 
+    /**
+     * Создает новый объект HashMap с начальной емкостью по умолчанию (16).
+     * Он инициализирует внутренний массив (bucket) размером 16 и все его элементы устанавливаются в null.
+     * Также инициализируется переменная size (размер) значением 0.
+     */
     public MyHashMap() {
         this(DEFAULT_CAPACITY);
     }
 
+    /**
+     * Создает пустой HashMap с указанной начальной емкостью.
+     * @param initialCapacity начальная емкость HashMap
+     */
     public MyHashMap(int initialCapacity) {
         buckets = new ArrayList<>(initialCapacity);
         for (int i = 0; i < initialCapacity; i++) {
@@ -19,7 +42,9 @@ public class MyHashMap<K, V> {
         }
         size = 0;
     }
-
+    /**
+     * Выводит на экран элементы HashMap в виде "ключ=значение"
+     */
     public void print(){
         for (LinkedList<Node<K, V>> bucket : buckets) {
             for (Node<K, V> node : bucket) {
@@ -27,7 +52,12 @@ public class MyHashMap<K, V> {
             }
         }
     }
-
+    /**
+     * Связывает указанное значение с указанным ключом в этом HashMap.
+     * Если карта ранее содержала значение для ключа, старое значение заменяется.
+     * @param key   ключ, с которым связывается указанное значение
+     * @param value значение, которое будет связано с указанным ключом
+     */
     public void put(K key, V value) {
         int bucketIndex = getBucketIndex(key);
         LinkedList<Node<K, V>> bucket = buckets.get(bucketIndex);
@@ -43,11 +73,12 @@ public class MyHashMap<K, V> {
             resize();
         }
     }
-
+    /**
+     * Возвращает значение, сопоставленное с указанным ключом, или null, если в этой карте отсутствует значение для ключа.
+     * @param key ключ, для которого должно быть возвращено сопоставленное значение
+     * @return значение, сопоставленное с указанным ключом, или null, если для ключа нет значения
+     */
     public V get(K key) {
-        if (key == null) {
-            throw new IllegalArgumentException("Key cannot be null.");
-        }
         int bucketIndex = getBucketIndex(key);
         LinkedList<Node<K, V>> bucket = buckets.get(bucketIndex);
         for (Node<K, V> node : bucket) {
@@ -57,11 +88,11 @@ public class MyHashMap<K, V> {
         }
         return null;
     }
-
+    /**
+     * Удаляет значение для указанного ключа из этой карты, если оно существует.
+     * @param key ключ, для которого нужно удалить значение
+     */
     public void remove(K key) {
-        if (key == null) {
-            throw new IllegalArgumentException("Key cannot be null.");
-        }
         int bucketIndex = getBucketIndex(key);
         LinkedList<Node<K, V>> bucket = buckets.get(bucketIndex);
         for (Node<K, V> node : bucket) {
@@ -72,11 +103,12 @@ public class MyHashMap<K, V> {
             }
         }
     }
-
+    /**
+     * Проверяет наличие значения, связанного с указанным ключом, HashMap
+     * @param key ключ, для которого нужно проверить наличие значения
+     * @return true, если значение существует, иначе false
+     */
     public boolean containsKey(K key) {
-        if (key == null) {
-            throw new IllegalArgumentException("Key cannot be null.");
-        }
         int bucketIndex = getBucketIndex(key);
         LinkedList<Node<K, V>> bucket = buckets.get(bucketIndex);
         for (Node<K, V> node : bucket) {
@@ -87,7 +119,11 @@ public class MyHashMap<K, V> {
         return false;
     }
 
-
+    /**
+     * Проверяет наличие указанного значения в HashMap
+     * @param value значение, для которого нужно проверить наличие
+     * @return true, если значение существует, иначе false
+     */
     public boolean containsValue(V value) {
         for (int i = 0; i < buckets.size(); i++) {
             LinkedList<Node<K, V>> bucket = buckets.get(i);
@@ -99,11 +135,17 @@ public class MyHashMap<K, V> {
         }
         return false;
     }
-
+    /**
+     * Проверяет, является ли HashMap пустым (не содержит пар ключ-значение).
+     * @return true, если HashMap пустой, в противном случае false
+     */
     public boolean isEmpty() {
         return size == 0;
     }
-
+    /**
+     * Возвращает количество пар ключ-значение в HashMap.
+     * @return количество пар ключ-значение в HashMap
+     */
     public int getSize() {
         return size;
     }
@@ -134,7 +176,9 @@ public class MyHashMap<K, V> {
         }
         buckets = newBuckets;
     }
-
+    /**
+     * Внутренний класс, представляющий узел HashMap.
+     */
     private static class Node<K, V> {
         private K key;
         private V value;
